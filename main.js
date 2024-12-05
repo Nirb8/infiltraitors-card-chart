dd0c2186da42c2384f54ad61ca6eac4bd9c8bd83
rhit-chanb <chan992599@gmail.com>
2024-12-04 22:44:37 -0500

    backup jsfiddle code and also muwahhaha it works

--- main.js
+++ main.js
@@ -1,3 +1,70 @@
+var lastClicked;
+var grid = clickableGrid(6,16,function(el,row,col,i){
+/*     console.log("You clicked on element:",el);
+    console.log("You clicked on row:",row);
+    console.log("You clicked on col:",col);
+    console.log("You clicked on item #:",i); */
+    console.log(el.classList);
+    console.log(Array.from(el.classList.values()));
+		if (el.classList.contains('clicked')) {
+    	el.classList.remove('clicked');
+    }
+    else
+    {
+    	 el.classList.add('clicked');
+    }
+    
+    if (row == 0 && col != 0){
+      console.log("number disable click");
+      var className = '.td-num-' + col;
+      /* console.log(className) */;
+      document.querySelectorAll(className).forEach(function(el) {
+            if (el.classList.contains('clicked')) {
+              el.classList.remove('clicked');
+            }
+        else
+        {
+          el.classList.add('clicked');
+        }
+      });
+      
+    }
+    
+    if (row != 0 && col == 0){
+    
+      var className = '';
+      switch(row) {
+          case 1:
+              className = '.red';
+              break;
+          case 2:
+              className = '.yellow';
+              break;
+          case 3:
+              className = '.green';
+              break;
+          case 4:
+              className = '.blue';
+              break;
+          case 5:
+              className = '.black';
+              break;
+      }
+      document.querySelectorAll(className).forEach(function(el) {
+            if (el.classList.contains('clicked')) {
+              el.classList.remove('clicked');
+            }
+        else
+        {
+          el.classList.add('clicked');
+        }
+      });
+      
+    }
+});
+
+document.body.appendChild(grid);
+     
 function clickableGrid( rows, cols, callback ){
     var i=0;
     var grid = document.createElement('table');
@@ -6,7 +73,31 @@ function clickableGrid( rows, cols, callback ){
         var tr = grid.appendChild(document.createElement('tr'));
         for (var c=0;c<cols;++c){
             var cell = tr.appendChild(document.createElement('td'));
-            cell.innerHTML = (++i - 1) % 15 + 1;
+            cell.innerHTML = (++i - 1) % 16;
+            if(cell.innerHTML == 0){
+            	cell.innerHTML = '';
+            } else {
+            	cell.classList.add('td-num-' + cell.innerHTML);
+            }
+            switch(r) {
+                case 1:
+                    cell.classList.add('red');
+                    break;
+                case 2:
+                    cell.classList.add('yellow');
+                    break;
+                case 3:
+                    cell.classList.add('green');
+                    break;
+                case 4:
+                    cell.classList.add('blue');
+                    break;
+                case 5:
+                    cell.classList.add('black');
+                    break;
+            }
+
+              
             
             cell.addEventListener('click',(function(el,r,c,i){
                 return function(){
@@ -17,21 +108,3 @@ function clickableGrid( rows, cols, callback ){
     }
     return grid;
 }
-
-
-var lastClicked;
-
-
-var grid = clickableGrid(4,15,function(el,row,col,i){
-    console.log("You clicked on element:",el);
-    console.log("You clicked on row:",row);
-    console.log("You clicked on col:",col);
-    console.log("You clicked on item #:",i);
-
-    el.className='clicked';
-    if (lastClicked) lastClicked.className='';
-    lastClicked = el;
-});
-
-document.body.appendChild(grid);
-     
